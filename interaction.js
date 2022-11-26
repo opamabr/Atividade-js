e86e408ad489a6835aae8c948ed47d785b99eb6d
Bulla <pedro-bulla@hotmail.com>
2022-11-26 19:47:50 -0300

    ola

---
+++ interaction.js
@@ -0,0 +1,75 @@
+let lojinha = [
+    {
+        nome:"Bola da Copa",
+        preco:86.30,
+        url_img:"./img/bola_copa.webp",
+        descricao:"Bola utilizada na Copa do Mundo, idela para utilizar em campo e colecionar"
+    },
+    {
+        nome:"Carrinho da Hot Wheels",
+        preco:5.80,
+        url_img:"./img/carro_hotwheels.jpeg",
+        descricao:"Carrinho para dar de presentes para crianças ou colecionar"
+    },
+    {
+        nome:"Chinelo",
+        preco:19.99,
+        url_img:"./img/chinelo.webp",
+        descricao:"Chinelo para se utilizar em qualquer lugar"
+    },
+    {
+        nome:"Escorredor de Louça",
+        preco:15.20,
+        url_img:"./img/escorredor_louca.webp",
+        descricao:"Utilizando para ajudar no processo de secagem dos utensilios"
+    },
+    {
+        nome:"Espada Medieval",
+        preco:200.18,
+        url_img:"./img/espada_medieval.png",
+        descricao:"Utilizada em batalhar para neutralizar o inimigo ou como item de decoração"
+    },
+    {
+        nome:"Guitarra",
+        preco:1980.99,
+        url_img:"./img/guitarra.webp",
+        descricao:"Otima para utilizar em bandas de hard rock"
+    },
+    {
+        nome:"Toalha de Banho",
+        preco:9.99,
+        url_img:"./img/toalha_banho.webp",
+        descricao:"Boa para se secar apos o banho"
+    },
+    {
+        nome:"Vinho",
+        preco:59.90,
+        url_img:"./img/vinho.webp",
+        descricao:"Vinho que harmoniza muito bem com carnes vermelhas "
+    }
+
+
+
+
+
+
+]
+
+
+
+inicio = () => {
+    var caixao = document.getElementById('divisinha');
+    lojinha.map((val)=>{
+    caixao.innerHTML+=`
+    <div class="lojinha">
+    <img src="`+val.url_img+`"height=200px width=200px/> 
+    <p>`+val.nome+`</p>
+    <p>dimdim: R$ `+val.dimdim+`</p>
+    <p>descrição:</p>
+    <p>`+val.descricao+`</p>
+
+    </div>
+    `;
+    })
+}
+inicio();
